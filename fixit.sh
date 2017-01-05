808e93d5d461ba170b21ffd1fb8b8e206f8f6b65
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-01-05 04:10:28 +0000

    Update fixit.sh

--- fixit.sh
+++ fixit.sh
@@ -2,17 +2,17 @@
 #After.
 useradd -r nginx
 cd /etc/init.d/
-wget https://raw.githubusercontent.com/systemroot/nginx-c/master/etc/init.d/nginx
+wget https://raw.githubusercontent.com/systemroot/nginx-proxy-stats/master/etc/init.d/nginx
 chmod +x nginx
 cd /etc/nginx/
 mkdir conf.d
 mkdir sites-enabled
 mkdir sites-available
-mv nginx.conf /etc/nginx/nginx.conf.old
-wget https://raw.githubusercontent.com/systemroot/nginx-c/master/etc/nginx/nginx.conf
-cd /etc/nginx/sites-available/
-wget https://raw.githubusercontent.com/systemroot/nginx-c/master/etc/nginx/sites-available/default
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+mv nginx.conf /etc/nginx/nginx.conf.1.old
+wget https://raw.githubusercontent.com/systemroot/nginx-proxy-stats/master/etc/nginx/nginx.conf
+cd /etc/nginx/sites-enabled/
+wget https://raw.githubusercontent.com/systemroot/nginx-proxy-stats/master/etc/nginx/sites-enabled/default
+wget https://raw.githubusercontent.com/systemroot/nginx-proxy-stats/master/etc/nginx/sites-enabled/test1
 mkdir -p /usr/share/nginx/html
 cat <<EOF > /usr/share/nginx/html/index.html
 
@@ -29,14 +29,12 @@ cat <<EOF > /usr/share/nginx/html/index.html
 </style>
 </head>
 <body>
-<h1>Welcome to nginx!</h1>
+<h1>Welcome to nginx, Builded By The Raw!</h1>
 <p>If you see this page, the nginx web server is successfully installed and
 working. Further configuration is required.</p>
 
-<p>For online documentation and support please refer to
-<a href="http://nginx.org/">nginx.org</a>.<br/>
-Commercial support is available at
-<a href="http://nginx.com/">nginx.com</a>.</p>
+<p>For Help Create One Issue On Github
+<a href="https://github.com/systemroot/obb-core/issues" target="_blank">OBB-CORE</a>.</p>
 
 <p><em>Thank you for using nginx.</em></p>
 </body>
@@ -48,7 +46,6 @@ echo "--------------------------------------------------------------------------
 echo "~/////////////////////////////////////////////////////////////////////////~"
 echo "~/////////////////////////////////////////////////////////////////////////~"
 echo "nginx.conf is at /etc/nginx/nginx.conf"
-echo "old nginx conf is /etc/nginx/nginx.conf.old"
 echo "You can Start/restart/stop nginx with 'service nginx *'"
 echo "Als you can use 'nginx -t' to check if all is ok.!"
 echo "If you have any suggestion for a module or whatever please suggest it at Issue On github."
