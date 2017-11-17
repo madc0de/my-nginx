9eaa151c3607817ae5929b539314e3bf1ba7094c
ƬHE ЯAW ☣ <raw@dopehosting.net>
2017-11-17 08:54:31 +0100

    Update get-mods.sh

--- nginx-as-stream-server/helper/get-mods.sh
+++ nginx-as-stream-server/helper/get-mods.sh
@@ -41,3 +41,4 @@
            git clone https://github.com/openresty/encrypted-session-nginx-module.git
            git clone https://github.com/Qihoo360/ngx_http_subrange_module.git
            git clone https://github.com/arut/nginx-rtmp-module.git
+           git clone https://github.com/aperezdc/ngx-fancyindex.git
